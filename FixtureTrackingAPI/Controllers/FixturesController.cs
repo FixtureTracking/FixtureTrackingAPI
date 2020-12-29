c96128e5843df0b9c9a90a844d6806eee7219df9
MustafaCEVIK <se.mustafacevik@gmail.com>
2020-12-30 00:23:53 +0300

    Added fixtures controller

---
+++ FixtureTrackingAPI/Controllers/FixturesController.cs
@@ -0,0 +1,65 @@
+﻿using FixtureTracking.Business.Abstract;
+using FixtureTracking.Entities.Concrete;
+using FixtureTracking.Entities.Dtos.Fixture;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace FixtureTrackingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FixturesController : ControllerBase
+    {
+        private readonly IFixtureService fixtureService;
+
+        public FixturesController(IFixtureService fixtureService)
+        {
+            this.fixtureService = fixtureService;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(Guid id)
+        {
+            var result = fixtureService.GetById(id);
+            if (result.Success)
+                return Ok(result);
+            return BadRequest(result.Message);
+        }
+
+        [HttpGet()]
+        public IActionResult GetList()
+        {
+            var result = fixtureService.GetList();
+            if (result.Success)
+                return Ok(result);
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost()]
+        public IActionResult Add(FixtureForAddDto fixtureForAddDto)
+        {
+            var result = fixtureService.Add(fixtureForAddDto);
+            if (result.Success)
+                return CreatedAtAction("GetById", new { id = result.Data }, result.Message);
+            return BadRequest(result.Message);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(Fixture fixture)
+        {
+            var result = fixtureService.Update(fixture);
+            if (result.Success)
+                return NoContent();
+            return BadRequest(result.Message);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            var result = fixtureService.Delete(id);
+            if (result.Success)
+                return NoContent();
+            return BadRequest(result.Message);
+        }
+    }
+}
