989e3553444db43428047e22c20ee6f0c2cd283e
MustafaCEVIK <se.mustafacevik@gmail.com>
2021-01-07 22:55:45 +0300

    Added auth controller, done startup configuration

---
+++ FixtureTrackingAPI/Controllers/AuthController.cs
@@ -0,0 +1,36 @@
+﻿using FixtureTracking.Business.Abstract;
+using FixtureTracking.Entities.Dtos.User;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FixtureTrackingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService authService;
+
+        public AuthController(IAuthService authService)
+        {
+            this.authService = authService;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login(UserForLoginDto userForLoginDto)
+        {
+            var result = authService.Login(userForLoginDto);
+            if (result.Success)
+                return Ok(result.Data);
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register(UserForRegisterDto userForRegisterDto)
+        {
+            var result = authService.Register(userForRegisterDto);
+            if (result.Success)
+                return CreatedAtAction("GetById", "Users", new { id = result.Data }, result.Message);
+            return BadRequest(result.Message);
+        }
+    }
+}
--- FixtureTrackingAPI/Startup.cs
+++ FixtureTrackingAPI/Startup.cs
@@ -1,16 +1,12 @@
+using FixtureTracking.Core.Utilities.Security.Encryption;
+using FixtureTracking.Core.Utilities.Security.Tokens;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace FixtureTrackingAPI
 {
@@ -23,18 +19,37 @@ namespace FixtureTrackingAPI
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowOrigin", builder => builder.AllowAnyOrigin());  // TODO : withorigin -> domain
+            });
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "FixtureTrackingAPI", Version = "v1" });
             });
+
+            var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = tokenOptions.Issuer,
+                    ValidAudience = tokenOptions.Audience,
+                    IssuerSigningKey = SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey),
+                };
+            });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -44,9 +59,13 @@ namespace FixtureTrackingAPI
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "FixtureTrackingAPI v1"));
             }
 
+            app.UseCors(options => options.AllowAnyOrigin().AllowAnyHeader());  // TODO : withorigin - allow header -> domain
+
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
